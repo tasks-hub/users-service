a29b525e04a36a5125eb7c3009dbacd8a6dc8f03
Farid Abulias Farias <fabulias@Farids-MacBook-Pro.local>
2024-01-12 02:30:28 -0300

    fix: goose migration

--- database/migrations/20240112002958_init.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-CREATE DATABASE users_db;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-DROP DATABASE users_db;
--- +goose StatementEnd
